91f691d5b7e22b3db4e874f9478a9058f4136951
TpacTrres <2pac.torres@gmail.com>
2024-01-01 22:35:01 +0100

    Add files via upload

---
+++ Franchise_audioPlayer.js
@@ -0,0 +1,18 @@
+﻿// JavaScript source code
+document.addEventListener("DOMContentLoaded", function () {
+    const audioPlayer = document.getElementById("audioPlayer");
+    const playPauseButton = document.getElementById("playPauseButton");
+
+    playPauseButton.addEventListener("click", togglePlayPause);
+    audioPlayer.addEventListener("timeupdate", updateProgressBar);
+
+    function togglePlayPause() {
+        if (audioPlayer.paused) {
+            audioPlayer.play();
+            playPauseButton.textContent = "⏸";
+        } else {
+            audioPlayer.pause();
+            playPauseButton.textContent = "▶";
+        }
+    }
+});
